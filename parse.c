9a90e348111d68b30fe29d673bd7f3c2437157ee
Krister <randyersson@gmail.com>
2023-04-16 19:58:47 +0200

    Add method to print XML node.

--- parse.c
+++ parse.c
@@ -1,5 +1,15 @@
 #include "parse.h"
 
+void print_element(xmlNodePtr node)
+{
+    if (node->type == XML_ELEMENT_NODE) {
+        printf("Element: %s\n", node->name);
+        if (node->children != NULL && node->children->type == XML_TEXT_NODE) {
+            printf("Value: %s\n", node->children->content);
+        }
+    }
+}
+
 void traverse_xml(xmlNodePtr node, void (*xml_operation)(xmlNodePtr))
 {
   for (xmlNodePtr child = node->children; child != NULL; child = child->next) {
