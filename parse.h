4b260a01ad6b58aa4ee705f3ee6754f3b2230b24
Krister <randyersson@gmail.com>
2023-04-16 19:58:00 +0200

    Add c file definitions.

---
+++ parse.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
+#define get_operaton "get"
+#define get_config_operaton "get-config"
+#define lock_operaton "lock"
+
+#define REQUEST "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\
+<rpc message-id=\"101\" xmlns=\"urn:ietf:params:xml:ns:netconf:base:1.0\">\
+  <get>\
+    <filter type=\"subtree\">\
+      <system-information xmlns=\"http://xml.juniper.net/xnm/1.1/xnm\">\
+        <hardware-model>SRX240H</hardware-model>\
+        <os-name>Junos</os-name>\
+        <os-version>12.1X47-D20.7</os-version>\
+      </system-information>\
+    </filter>\
+  </get>\
+</rpc>"
+
+
+void traverse_xml(xmlNodePtr node, void (*xml_operation)(xmlNodePtr));
