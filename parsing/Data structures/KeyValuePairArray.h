60e4fda90eb9518ada64c0fd2429fceb7c90943e
Krister <randyersson@gmail.com>
2023-04-19 16:02:15 +0200

    Update init for array to take a size argument.

--- parsing/Data structures/KeyValuePairArray.h
+++ parsing/Data structures/KeyValuePairArray.h
@@ -16,7 +16,7 @@ typedef struct KeyValuePair {
     void *value;
 } KeyValuePair;
 
-int init_key_value_array(KeyValuePairArray *array);
+int init_key_value_array(KeyValuePairArray *array, unsigned char capacity);
 int init_key_value_pair(KeyValuePair *kv, const char *key, const void *value);
 int add_key_value(KeyValuePairArray *array, const KeyValuePair *kv);
 int free_key_value_pair_array(KeyValuePairArray *array);
