6c9238babc6a72ae4764c243bcf1b63f8370af42
Krister <randyersson@gmail.com>
2023-04-19 15:47:24 +0200

    Add Key:Value array definitions

---
+++ parsing/Data structures/KeyValuePairArray.c
@@ -0,0 +1,22 @@
+#include "KeyValuePairArray.h"
+
+
+int init_key_value_array(KeyValuePairArray *array) {
+
+
+}
+
+int init_key_value_pair(KeyValuePair *kv, const char *key, const void *value) {
+    
+
+}
+
+int add_key_value(KeyValuePairArray *array, const KeyValuePair *kv) {
+
+
+}
+
+int free_key_value_pair_array(KeyValuePairArray *array) {
+
+
+}
---
+++ parsing/Data structures/KeyValuePairArray.h
@@ -0,0 +1,24 @@
+#ifndef KEY_VALUE_PAIR_API_H
+#define KEY_VALUE_PAIR_API_H
+
+#include <stddef.h>
+
+#define MAX_CAPACITY 10
+
+typedef struct KeyValuePairArray {
+    KeyValuePair *data;
+    size_t size;
+    size_t capacity;
+} KeyValuePairArray;
+
+typedef struct KeyValuePair {
+    char *key;
+    void *value;
+} KeyValuePair;
+
+void init_key_value_array(KeyValuePairArray *array);
+void init_key_value_pair(KeyValuePair *kv, const char *key, const void *value);
+void add_key_value(KeyValuePairArray *array, const KeyValuePair *kv);
+void free_key_value_pair_array(KeyValuePairArray *array);
+
+#endif
