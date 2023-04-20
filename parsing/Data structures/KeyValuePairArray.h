18eff3f680d5c24291404944b24df08055ce6ad1
Krister <randyersson@gmail.com>
2023-04-20 13:40:57 +0200

    Changed ordering of structs to avoid errors.

--- parsing/Data structures/KeyValuePairArray.h
+++ parsing/Data structures/KeyValuePairArray.h
@@ -2,10 +2,19 @@
 #define KEY_VALUE_PAIR_API_H
 
 #include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #define MAX_CAPACITY 10
 #define SUCCESS      0
 #define FAIL         -1
+
+
+typedef struct KeyValuePair {
+    char *key;
+    void *value;
+} KeyValuePair;
 
 typedef struct KeyValuePairArray {
     KeyValuePair *data;
@@ -13,14 +22,12 @@ typedef struct KeyValuePairArray {
     size_t capacity;
 } KeyValuePairArray;
 
-typedef struct KeyValuePair {
-    char *key;
-    void *value;
-} KeyValuePair;
+
 
 int init_key_value_array(KeyValuePairArray *array, unsigned char capacity);
 int init_key_value_pair(KeyValuePair *kv, const char *key, const void *value, size_t value_size);
 int add_key_value(KeyValuePairArray *array, const KeyValuePair *kv);
 int free_key_value_pair_array(KeyValuePairArray *array);
+int print_all_nodes(KeyValuePairArray *array);
 
 #endif
