e8693f0012f453afd1b4d2dbde8126bdd2e918dd
Krister <randyersson@gmail.com>
2023-05-01 15:17:10 +0200

    Add init test.

--- tests/unitTests.c
+++ tests/unitTests.c
@@ -11,6 +11,14 @@ void tearDown(void) {
 }
 
 
+void test_init_key_value_array(void) {
+    KeyValuePairArray array;
+    TEST_ASSERT_EQUAL(SUCCESS, init_key_value_array(&array, 5));
+    TEST_ASSERT_EQUAL(5, array.capacity);
+    TEST_ASSERT_EQUAL(0, array.size);
+    TEST_ASSERT_NOT_NULL(array.data);
+    free_key_value_pair_array(&array);
+}
 
 
 
