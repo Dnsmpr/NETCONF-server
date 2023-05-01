fd15f7abab88fde1619e534269915c41f2b79174
Krister <randyersson@gmail.com>
2023-05-01 15:17:44 +0200

    Add KeyValuePair init test.

--- tests/unitTests.c
+++ tests/unitTests.c
@@ -20,6 +20,25 @@ void test_init_key_value_array(void) {
     free_key_value_pair_array(&array);
 }
 
+void test_init_key_value_pair(void) {
+    KeyValuePair kv;
+    TEST_ASSERT_EQUAL(SUCCESS, init_key_value_pair(&kv, "test_key", "test_value", strlen("test_value") + 1));
+    TEST_ASSERT_EQUAL_STRING("test_key", kv.key);
+    TEST_ASSERT_EQUAL_STRING("test_value", (char *)kv.value);
+    free(kv.key);
+    free(kv.value);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_init_key_value_array);
+    RUN_TEST(test_init_key_value_pair);
+    // Add more RUN_TEST lines for the rest of the test functions
+    return UNITY_END();
+}
+
+
+// Add more test functions for the rest of the functions in your implementation
 
 
 
